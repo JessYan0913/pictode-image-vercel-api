66622abcf0c29c3e6796ec2390975f6ebef22dc7
JessYan0913 <1141326491@qq.com>
2024-10-09 14:45:53 +0800

    feat: 增加file上传逻辑

--- api/app.py
+++ api/app.py
@@ -1,45 +1,69 @@
-from flask import Flask, request
+import os
+from flask import Flask, flash, request, redirect, url_for, send_from_directory
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = "../files"
+ALLOWED_EXTENSIONS = {"txt", "pdf", "png", "jpg", "jpeg", "gif"}
 
 app = Flask(__name__)
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+app.config["MAX_CONTENT_LENGTH"] = 16 * 1000 * 1000
 
 
 @app.route("/")
 def home():
-    return "Hello, World!"
+    return "Hello, World ddd"
 
 
-@app.route("/get", methods=["GET"])
-def get():
-    return "welcome, you could get someting'"
+def allowed_file(filename):
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route("/post", methods=["POST"])
-def post():
-    data = request.get_json()
-    return {"whatyoupost": data}
+@app.route("/file", methods=["GET", "POST"])
+def upload_file():
+    if request.method == "POST":
+        # check if the post request has the file part
+        if "file" not in request.files:
+            flash("No file part")
+            return redirect(request.url)
+        file = request.files["file"]
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == "":
+            flash("No selected file")
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+            return redirect(url_for("download_file", name=filename))
+    return """
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    """
 
 
-@app.route("/double", methods=("GET", "POST"))
-def double():
-
-    if request.method == "GET":
-        return {"getmsg": "you could even deal with them together'"}
-
-    elif request.method == "POST":
-        data = request.get_json()
-        return {"postmsg": data}
+@app.route("/uploads/<name>")
+def download_file(name):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
 
 
 # register bluepirnt
 from . import blueprint
+
 app.register_blueprint(blueprint.bp)
 
 from . import libhandler
+
 app.register_blueprint(libhandler.bp)
 
 # 锁定输出格式为 utf-8
-app.config['JSON_AS_ASCII'] = False
-app.config['JSONIFY_MIMETYPE'] = "application/json;charset=utf-8"
+app.config["JSON_AS_ASCII"] = False
+app.config["JSONIFY_MIMETYPE"] = "application/json;charset=utf-8"
 
 if __name__ == "__main__":
     app.run(debug=False)
